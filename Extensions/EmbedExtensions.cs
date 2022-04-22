90b4a9b2a838b429a1a9c0ee87b9610ff18446e7
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-04-22 22:28:41 +0100

    Create EmbedExtensions.cs

---
+++ Extensions/EmbedExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+
+namespace Magus.Bot.Extensions
+{
+    public static class EmbedExtensions
+    {
+        public static Discord.Embed CreateDiscordEmbed(this Data.Models.Embeds.Embed embed)
+        {
+            var discordEmbed = new EmbedBuilder()
+            {
+                Title = embed.Title,
+                Description = embed.Description,
+                Url = embed.Url,
+                ImageUrl = embed.ImageUrl,
+                ThumbnailUrl = embed.ThumbnailUrl,
+                Color = embed.ColorRaw,
+                Timestamp = embed.Timestamp,
+            };
+
+            if (embed.Fields != null && embed.Fields.Any())
+            {
+                var fields = new List<EmbedFieldBuilder>();
+                foreach (var field in embed.Fields)
+                {
+                    fields.Add(new EmbedFieldBuilder() { Name = field.Name, IsInline = field.IsInline, Value = field.Value });
+                }
+                discordEmbed.Fields = fields;
+            }
+
+            if (embed.Footer != null)
+            {
+                discordEmbed.Footer = new() { Text = embed.Footer?.Text, IconUrl = embed.Footer?.IconUrl };
+            }
+
+            return discordEmbed.Build();
+        }
+    }
+}
