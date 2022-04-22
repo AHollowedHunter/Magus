c5aaaae1df0883754b706558c539d668571e763f
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-04-22 21:45:00 +0100

    Add bot core classes

---
+++ Bot.cs
@@ -0,0 +1,81 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Magus.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Magus.Bot
+{
+    class Bot
+    {
+        private static IConfiguration configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "MAGUS_")
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets<Bot>()
+                .Build();
+
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            using var services = ConfigureServices();
+
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            var commands = services.GetRequiredService<InteractionService>();
+
+            client.Log += LogAsync;
+            commands.Log += LogAsync;
+
+            // Slash Commands and Context Commands are can be automatically registered, but this process needs to happen after the client enters the READY state.
+            // Since Global Commands take around 1 hour to register, we should use a test guild to instantly update and test our commands. To determine the method we should
+            // register the commands with, we can check whether we are in a DEBUG environment and if we are, we can register the commands to a predetermined test guild.
+            client.Ready += async () =>
+            {
+                if (IsDebug())
+                    await commands.RegisterCommandsToGuildAsync(configuration.GetValue<ulong>("testGuild"), true);
+                else
+                    await commands.RegisterCommandsGloballyAsync(true);
+            };
+
+            // Here we can initialize the service that will register and execute our commands
+            await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+            await client.LoginAsync(TokenType.Bot, configuration["BotToken"]);
+            await client.StartAsync();
+
+            await client.SetGameAsync(name: "use \"!help\" or \"/magus help\" for help!", type: ActivityType.Playing);
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        static Task LogAsync(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
+        static ServiceProvider ConfigureServices()
+            => new ServiceCollection()
+                .AddSingleton(configuration)
+                .AddSingleton<IDatabaseService>(x => new LiteDBService(configuration.GetSection("DatabaseService")))
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+
+        static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
---
+++ CommandHandler.cs
@@ -0,0 +1,199 @@
+﻿using Discord;
+using Discord.Commands;
+using CommandIResult = Discord.Commands.IResult;
+using InteractionsIResult = Discord.Interactions.IResult;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace Magus.Bot
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _textCommands;
+        private readonly InteractionService _intCommands;
+        private readonly IServiceProvider _services;
+
+        public CommandHandler(DiscordSocketClient client, CommandService textCommands, InteractionService intCommands, IServiceProvider services)
+        {
+            _client = client;
+            _textCommands = textCommands;
+            _intCommands = intCommands;
+            _services = services;
+        }
+
+        public async Task InitializeAsync()
+        {
+            // Add the public modules that inherit InteractionModuleBase<T> to the InteractionService
+            await _intCommands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _textCommands.AddModulesAsync(Assembly.GetExecutingAssembly(), _services);
+            // Another approach to get the assembly of a specific type is:
+            // typeof(CommandHandler).Assembly
+
+
+            // Process the InteractionCreated payloads to execute Interactions commands
+            _client.InteractionCreated += HandleInteraction;
+
+            // Process the command execution results 
+            _intCommands.SlashCommandExecuted += SlashCommandExecuted;
+            _intCommands.ContextCommandExecuted += ContextCommandExecuted;
+            _intCommands.ComponentCommandExecuted += ComponentCommandExecuted;
+
+            _textCommands.CommandExecuted += CommandExecutedAsync;
+
+            _client.MessageReceived += MessageReceivedAsync;
+
+        }
+
+        # region Error Handling
+
+        private Task ComponentCommandExecuted(ComponentCommandInfo arg1, IInteractionContext arg2, InteractionsIResult arg3)
+        {
+            if (!arg3.IsSuccess)
+            {
+                switch (arg3.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task ContextCommandExecuted(ContextCommandInfo arg1, IInteractionContext arg2, InteractionsIResult arg3)
+        {
+            if (!arg3.IsSuccess)
+            {
+                switch (arg3.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task SlashCommandExecuted(SlashCommandInfo arg1, IInteractionContext arg2, InteractionsIResult arg3)
+        {
+            if (!arg3.IsSuccess)
+            {
+                switch (arg3.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+        # endregion
+
+        # region Execution
+
+        private async Task HandleInteraction(SocketInteraction arg)
+        {
+            try
+            {
+                var ctx = new SocketInteractionContext(_client, arg);
+
+                await _intCommands.ExecuteCommandAsync(ctx, _services);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                // If a Slash Command execution fails it is most likely that the original interaction acknowledgement will persist. It is a good idea to delete the original
+                // response, or at least let the user know that something went wrong during the command execution.
+                if (arg.Type == InteractionType.ApplicationCommand)
+                    await arg.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
+            }
+        }
+
+        public async Task MessageReceivedAsync(SocketMessage rawMessage)
+        {
+            // Ignore system messages, or messages from other bots
+            if (!(rawMessage is SocketUserMessage message))
+                return;
+            if (message.Source != MessageSource.User)
+                return;
+
+            // This value holds the offset where the prefix ends
+            var argPos = 0;
+
+            if (!message.HasCharPrefix('!', ref argPos))
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+            // Perform the execution of the command. In this method,
+            // the command service will perform precondition and parsing check
+            // then execute the command if one is matched.
+            await _textCommands.ExecuteAsync(context, argPos, _services);
+            // Note that normally a result will be returned by this format, but here
+            // we will handle the result in CommandExecutedAsync,
+        }
+
+        public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, CommandIResult result)
+        {
+            // command is unspecified when there was a search failure (command not found); we don't care about these errors
+            if (!command.IsSpecified)
+                return;
+
+            // the command was successful, we don't care about this result, unless we want to log that a command succeeded.
+            if (result.IsSuccess)
+                return;
+
+            // the command failed, let's notify the user that something happened.
+            await context.Channel.SendMessageAsync($"error: {result}");
+        }
+
+        # endregion
+    }
+}
