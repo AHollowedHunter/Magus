a3ef422c0dac332db902c5185b0e0f105ad9f770
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-08-26 00:43:04 +0100

    create KVObject extension methods

---
+++ Extensions/KVObjectExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using ValveKeyValue;
+
+namespace Magus.DataBuilder.Extensions
+{
+    public static class KVObjectExtensions
+    {
+        public static T ParseEnum<T>(this KVObject? kvObject) where T : struct
+        {
+            if (kvObject == null)
+                return default(T);
+
+            var value = kvObject.Value.ToString() ?? "";
+            value = value.Replace("|", ",");
+            Enum.TryParse<T>(value, true, out var result);
+            return result;
+        }
+
+        public static IEnumerable<T> ParseList<T>(this KVObject? kvObject) where T : IConvertible
+        {
+            List<T> result = new();
+            if (kvObject == null)
+                return result;
+
+            var value = kvObject.Value.ToString() ?? "";
+            var separators = new Regex("[,;\\s]+");
+            var values = separators.Split(value);
+            result.AddRange(values.Select(x => (T)Convert.ChangeType(x, typeof(T))).ToList());
+
+            return result;
+        }
+
+        public static T? ParseValue<T>(this KVObject? kvObject) where T : IConvertible
+            => (T)Convert.ChangeType(kvObject?.Value!, typeof(T));
+    }
+}
