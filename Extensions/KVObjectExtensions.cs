d22e86986a4990df420ba97b4040df8c0b1593d0
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-08-27 00:59:19 +0100

    improve kvobject extensions

--- Extensions/KVObjectExtensions.cs
+++ Extensions/KVObjectExtensions.cs
@@ -6,31 +6,35 @@ namespace Magus.DataBuilder.Extensions
 {
     public static class KVObjectExtensions
     {
-        public static T ParseEnum<T>(this KVObject? kvObject) where T : struct
-        {
-            if (kvObject == null)
-                return default(T);
-
-            var value = kvObject.Value.ToString() ?? "";
-            value = value.Replace("|", ",");
-            Enum.TryParse<T>(value, true, out var result);
-            return result;
-        }
-
         /// <summary>
-        /// Use for single values when expected
+        /// Use for a single key value when expected
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="kvObject"></param>
         /// <param name="name"></param>
         /// <param name="defaultValue"></param>
         /// <returns></returns>
-        public static T? ParseChildValue<T>(this KVObject kvObject, string name, T? defaultValue = default) where T: IConvertible
+        public static T? ParseChildValue<T>(this KVObject kvObject, string name, T defaultValue = default) where T : IConvertible
         {
             var child = kvObject.Children.FirstOrDefault(x => x.Name == name);
             if (child == null)
                 return defaultValue;
             return child.ParseValue<T>();
+        }
+
+        /// <summary>
+        /// Use for a single key value when expected
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="kvObject"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static TEnum ParseChildEnum<TEnum>(this KVObject kvObject, string name) where TEnum : struct, Enum
+        {
+            var child = kvObject.Children.FirstOrDefault(x => x.Name == name);
+            if (child == null)
+                return default;
+            return child.ParseEnum<TEnum>();
         }
 
         /// <summary>
@@ -41,13 +45,30 @@ namespace Magus.DataBuilder.Extensions
         /// <param name="name"></param>
         /// <param name="defaultValue"></param>
         /// <returns></returns>
-        public static IEnumerable<T> ParseChildList<T>(this KVObject kvObject, string name, IEnumerable<T> defaultValue = null!) where T: IConvertible
+        public static IEnumerable<T> ParseChildValueList<T>(this KVObject kvObject, string name, IEnumerable<T> defaultValue = null!) where T : IConvertible
         {
             var child = kvObject.Children.FirstOrDefault(x => x.Name == name);
-            if (child == null)
+            if (child == null)  
                 return Enumerable.Empty<T>();
 
             return child.ParseList<T>();
+        }
+
+        /// <summary>
+        /// Use for a list of values when expected
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="kvObject"></param>
+        /// <param name="name"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static IEnumerable<TEnum> ParseChildEnumList<TEnum>(this KVObject kvObject, string name) where TEnum : struct, Enum
+        {
+            var child = kvObject.Children.FirstOrDefault(x => x.Name == name);
+            if (child == null)  
+                return Enumerable.Empty<TEnum>();
+
+            return child.ParseEnumList<TEnum>();
         }
 
         public static IEnumerable<T> ParseList<T>(this KVObject? kvObject) where T : IConvertible
@@ -62,20 +83,53 @@ namespace Magus.DataBuilder.Extensions
 
             foreach (var value in values)
             {
-                result.Add(ParseValue<T>(value) ?? default!);
+                result.Add(ParseValue<T>(value));
             }
+            return result;
+        }
 
+        /// <summary>
+        /// Parse a list of enums in the order they were stored
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="kvObject"></param>
+        /// <returns></returns>
+        public static IEnumerable<TEnum> ParseEnumList<TEnum>(this KVObject? kvObject) where TEnum : struct, Enum
+        {
+            List<TEnum> result = new();
+            if (kvObject == null)
+                return result;
+
+            var kvValue    = kvObject.Value.ToString() ?? "";
+            var separators = new Regex("[,;|\\s]+");
+            var values     = separators.Split(kvValue);
+
+            foreach (var value in values)
+            {
+                result.Add(ParseEnum<TEnum>(value));
+            }
             return result;
         }
 
         public static T? ParseValue<T>(this KVObject? kvObject) where T : IConvertible
-            => ParseValue<T>(kvObject?.Value);
+            => kvObject == null ? default : kvObject.Value.ParseValue<T>();
 
-        private static T? ParseValue<T>(this KVValue? obj) where T : IConvertible
+        public static T ParseValue<T>(this KVValue? obj) where T : IConvertible
+            => (T)Convert.ChangeType(obj!, typeof(T));
+        
+
+        public static T ParseEnum<T>(this KVObject? kvObject) where T : struct, Enum
+            => kvObject?.Value.ParseEnum<T>() ?? default;
+
+        public static T ParseEnum<T>(this KVValue? kvValue) where T : struct, Enum
         {
-            if (typeof(T).IsEnum)
-                return (T)Enum.Parse(typeof(T), ParseValue<string>(obj)!);
-            return (T)Convert.ChangeType(obj!, typeof(T));
+            if (kvValue == null)
+                return default;
+
+            var value = kvValue.ToString() ?? "";
+            value     = value.Replace("|", ",");
+            Enum.TryParse<T>(value, true, out var result);
+            return result;
         }
     }
 }
