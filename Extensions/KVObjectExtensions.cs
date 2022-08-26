8a59660fa3c71a15400ec94f40b125cf8fd48616
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-08-26 16:55:28 +0100

    kvobject extension improvements
    update ValveKeyValue to parse csv numbers properly

--- Extensions/KVObjectExtensions.cs
+++ Extensions/KVObjectExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
 using ValveKeyValue;
 
 namespace Magus.DataBuilder.Extensions
@@ -16,21 +17,65 @@ namespace Magus.DataBuilder.Extensions
             return result;
         }
 
+        /// <summary>
+        /// Use for single values when expected
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="kvObject"></param>
+        /// <param name="name"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static T? ParseChildValue<T>(this KVObject kvObject, string name, T? defaultValue = default) where T: IConvertible
+        {
+            var child = kvObject.Children.FirstOrDefault(x => x.Name == name);
+            if (child == null)
+                return defaultValue;
+            return child.ParseValue<T>();
+        }
+
+        /// <summary>
+        /// Use for a list of values when expected
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="kvObject"></param>
+        /// <param name="name"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ParseChildList<T>(this KVObject kvObject, string name, IEnumerable<T> defaultValue = null!) where T: IConvertible
+        {
+            var child = kvObject.Children.FirstOrDefault(x => x.Name == name);
+            if (child == null)
+                return Enumerable.Empty<T>();
+
+            return child.ParseList<T>();
+        }
+
         public static IEnumerable<T> ParseList<T>(this KVObject? kvObject) where T : IConvertible
         {
             List<T> result = new();
             if (kvObject == null)
                 return result;
 
-            var value = kvObject.Value.ToString() ?? "";
+            var kvValue    = kvObject.Value.ToString() ?? "";
             var separators = new Regex("[,;\\s]+");
-            var values = separators.Split(value);
-            result.AddRange(values.Select(x => (T)Convert.ChangeType(x, typeof(T))).ToList());
+            var values     = separators.Split(kvValue);
+
+            foreach (var value in values)
+            {
+                result.Add(ParseValue<T>(value) ?? default!);
+            }
 
             return result;
         }
 
         public static T? ParseValue<T>(this KVObject? kvObject) where T : IConvertible
-            => (T)Convert.ChangeType(kvObject?.Value!, typeof(T));
+            => ParseValue<T>(kvObject?.Value);
+
+        private static T? ParseValue<T>(this KVValue? obj) where T : IConvertible
+        {
+            if (typeof(T).IsEnum)
+                return (T)Enum.Parse(typeof(T), ParseValue<string>(obj)!);
+            return (T)Convert.ChangeType(obj!, typeof(T));
+        }
     }
 }
