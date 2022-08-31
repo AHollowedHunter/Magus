66b1adf62f4b5546b019b1f5a8b2c54639f7b980
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-09-01 00:25:06 +0100

    update info module

--- Modules/InfoModule.cs
+++ Modules/InfoModule.cs
@@ -21,16 +21,16 @@ namespace Magus.Bot.Modules
         [SlashCommand("hero", "🎶 I need a hero 🎶")]
         public async Task InfoHero([Autocomplete(typeof(HeroAutocompleteHandler))] int id)
         {
-            var heroInfo = _db.GetEntityInfo<HeroInfo>(id);
+            var heroInfo = _db.GetEntityInfo<HeroInfoEmbed>(id, Context.Interaction.UserLocale);
 
             await RespondAsync(embed: heroInfo.Embed.CreateDiscordEmbed());
         }
 
         //Disabled for now
-        //[SlashCommand("ability", "Ahh. How does this one work?")]
+        [SlashCommand("ability", "Ahh. How does this one work?")]
         public async Task InfoAbility([Autocomplete(typeof(AbilityAutocompleteHandler))] int id)
         {
-            var abilityInfo = _db.GetEntityInfo<AbilityInfo>(id);
+            var abilityInfo = _db.GetEntityInfo<AbilityInfoEmbed>(id, Context.Interaction.UserLocale);
 
             await RespondAsync(embed: abilityInfo.Embed.CreateDiscordEmbed());
         }
@@ -38,7 +38,7 @@ namespace Magus.Bot.Modules
         [SlashCommand("item", "Living in a material world")]
         public async Task InfoItem([Autocomplete(typeof(ItemAutocompleteHandler))] int id)
         {
-            var itemInfo = _db.GetEntityInfo<ItemInfo>(id);
+            var itemInfo = _db.GetEntityInfo<ItemInfoEmbed>(id, Context.Interaction.UserLocale);
 
             await RespondAsync(embed: itemInfo.Embed.CreateDiscordEmbed());
         }
