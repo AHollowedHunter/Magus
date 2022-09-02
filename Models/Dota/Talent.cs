7f3a78da19542c3fa8407d9fa5e090f84c98713d
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-09-02 17:20:52 +0100

    improve talent postion

--- Models/Dota/Talent.cs
+++ Models/Dota/Talent.cs
@@ -9,6 +9,11 @@
         public string Note { get; set; }
         public AbilityType AbilityType { get; set; }
         public AbilityBehavior AbilityBehavior { get; set; }
+        /// <summary>
+        /// Level is which XP level the talent is for
+        /// Side is the side it appears, 0 is right, 1 is left (Thanks volvo for right talent first)
+        /// </summary>
+        public (byte Level, byte Side) Position { get; set; }
 
         public string? AdLinkedAbilities { get; set; } // Sometimes set here...
 
@@ -20,6 +25,7 @@
             public string Value { get; set; }
             public string? AdLinkedAbilities { get; set; }
         }
+
 
         // for empty talents, in abilities scan for talent name, then add the value if missing
     }
