5ff163fe06e0920663080a5506464f9834cbcfc2
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-09-04 14:15:10 +0100

    Entity Lore embeds

---
+++ Models/Embeds/Lore/EntityLoreEmbed.cs
@@ -0,0 +1,14 @@
+﻿namespace Magus.Data.Models.Embeds
+{
+    public abstract record EntityLoreEmbed : ISnowflakeRecord, ILocaleRecord, INamedEntity
+    {
+        public ulong Id { get; set; }
+        public int EntityId { get; set; }
+        public string Locale { get; set; }
+        public string InternalName { get; set; }
+        public string Name { get; set; }
+        public string? RealName { get; set; }
+        public IEnumerable<string>? Aliases { get; set; }
+        public Embed Embed { get; set; }
+    }
+}
