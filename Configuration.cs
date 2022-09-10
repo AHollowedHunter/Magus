59728da4ba6825add68d7e94150e4ad2233e0a92
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-09-10 20:47:05 +0100

    Add general configuration

---
+++ Configuration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Magus.Common
+{
+    public class Configuration
+    {
+        public Configuration()
+        {
+
+        }
+
+        public Configuration(IConfiguration config)
+        {
+            config.Bind(this);
+        }
+
+        public LocalisationConfig Localisation { get; set; }
+        public DatabaseConfig DatabaseService { get; set; }
+
+        public class LocalisationConfig
+        {
+            public string DefaultLanguage { get; set; }
+            public Dictionary<string, string[]> SourceLocaleMappings { get; set; }
+        }
+        
+        public class DatabaseConfig
+        {
+            public string ConnectionString { get; set; }
+        }
+    }
+}
