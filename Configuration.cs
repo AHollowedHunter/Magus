ca12c3a01ecc98eb719c6da26b8bd6d78f9c06cf
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-09-11 20:42:23 +0100

    Update Configuration.cs

--- Configuration.cs
+++ Configuration.cs
@@ -16,17 +16,31 @@ namespace Magus.Common
 
         public LocalisationConfig Localisation { get; set; }
         public DatabaseConfig DatabaseService { get; set; }
+        public SteamConfig Steam { get; set; }
+
+        public string BotToken { get; set; }
+
+        public string BotInvite { get; set; }
+        public string BotServer { get; set; }
 
         public class LocalisationConfig
         {
             public string DefaultLanguage { get; set; }
             public Dictionary<string, string[]> SourceLocaleMappings { get; set; }
+
+            public IList<string> Locales
+                => SourceLocaleMappings.SelectMany(x => x.Value).ToList();
         }
         
         public class DatabaseConfig
         {
             public string ConnectionString { get; set; }
             public string DatabaseName { get; set; }
+        }
+
+        public class SteamConfig
+        {
+            public string SteamKey { get; set; }
         }
     }
 }
