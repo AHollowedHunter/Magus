4386b4e6a0319cd7a0f749be0143d4d7212641af
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-09-19 21:55:54 +0100

    Opendota modesl

---
+++ Models/OpenDota/League.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Magus.Data.Models.OpenDota
+{
+    public sealed record League
+    {
+        [JsonPropertyName("leagueid")]
+        public long Leagueid { get; set; }
+        [JsonPropertyName("ticket")]
+        public string Ticket { get; set; }
+        [JsonPropertyName("banner")]
+        public string Banner { get; set; }
+        [JsonPropertyName("tier")]
+        public string Tier { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
