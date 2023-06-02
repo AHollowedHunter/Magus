cb9ea598e92ea3ff85ae038b68aed56ec2c3062b
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2023-06-02 23:28:16 +0100

    hero icon emotes

---
+++ src/Magus.Common/Attributes/EntityEmoteAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Magus.Common.Attributes
+{
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class EntityEmoteAttribute : Attribute
+    {
+        /// <summary>
+        /// Mark an emote with the entity id
+        /// </summary>
+        /// <param name="entityId">The entities ID</param>
+        public EntityEmoteAttribute(int entityId)
+        {
+            EntityId = entityId;
+        }
+
+        /// <summary>
+        /// The Entities ID
+        /// </summary>
+        public int EntityId { get; }
+    }
+}
