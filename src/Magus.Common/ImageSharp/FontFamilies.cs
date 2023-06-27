af194ea541998c9ed1d6a6024ad2c105c9d0ef75
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2023-06-27 12:05:41 +0100

    remove redundant namespace

--- src/Magus.Common/ImageSharp/FontFamilies.cs
+++ src/Magus.Common/ImageSharp/FontFamilies.cs
@@ -13,24 +13,24 @@ namespace Magus.Common.ImageSharp
 
         static void AddNotoSans()
         {
-            fontCollection.Add(Common.Fonts.NotoSans_Black.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_BlackItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_Bold.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_BoldItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_ExtraBold.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_ExtraBoldItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_ExtraLight.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_ExtraLightItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_Italic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_Light.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_LightItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_Medium.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_MediumItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_Regular.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_SemiBold.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_SemiBoldItalic.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_Thin.AsMemoryStream());
-            fontCollection.Add(Common.Fonts.NotoSans_ThinItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Black.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_BlackItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Bold.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_BoldItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_ExtraBold.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_ExtraBoldItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_ExtraLight.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_ExtraLightItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Italic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Light.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_LightItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Medium.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_MediumItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Regular.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_SemiBold.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_SemiBoldItalic.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_Thin.AsMemoryStream());
+            fontCollection.Add(Fonts.NotoSans_ThinItalic.AsMemoryStream());
         }
 
         public static FontFamily NotoSans => fontCollection.Get("Noto Sans");
