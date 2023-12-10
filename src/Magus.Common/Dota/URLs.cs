efd0241fd5308edb9eb2c8ac20502bfcbafdb2ac
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2023-12-10 16:22:19 +0000

    Update URLs.cs

--- src/Magus.Common/Dota/URLs.cs
+++ src/Magus.Common/Dota/URLs.cs
@@ -5,7 +5,8 @@ namespace Magus.Common.Dota;
 
 public static class URLs
 {
-    public static string BaseUrl => "https://cdn.cloudflare.steamstatic.com/apps/dota2/";
+    public static string SteamCDN => "https://cdn.cloudflare.steamstatic.com";
+    public static string BaseUrl => SteamCDN + "/apps/dota2/";
     public static string Hero => BaseUrl + "images/dota_react/heroes/";
     public static string HeroCrop => BaseUrl + "images/dota_react/heroes/crops/";
     public static string Ability => BaseUrl + "images/dota_react/abilities/";
@@ -26,25 +27,29 @@ public static class URLs
     public static string TurnRateIcon => BaseUrl + "images/dota_react/heroes/stats/icon_turn_rate.png";
     public static string VisionIcon => BaseUrl + "images/dota_react/heroes/stats/icon_vision.png";
 
-    public static string DotaColourLogo => "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/footer_logo.png";
-    public static string DotaWhiteLogo => "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_horiz.png";
+    public static string DotaColourLogo => BaseUrl + "/images/dota_react/footer_logo.png";
+    public static string DotaWhiteLogo => BaseUrl + "/images/dota_react/global/dota2_logo_horiz.png";
 
-    public static string GetHeroUrl(string heroName) => $"https://www.dota2.com/hero/{Regex.Replace(heroName.ToLower(), @"[^a-zA-Z0-9-']", string.Empty)}";
-    public static string GetHeroImage(string internalName) => $"{Hero}{internalName[14..]}.png"; // ignore "npc_dota_hero_" from internal name
-    public static string GetAbilityImage(string internalName) => $"{Ability}{internalName}.png";
-    public static string GetItemImage(string internalName) => $"{Item}{internalName[5..]}.png"; // ignore "item_" from internal name
+    public static string GetHeroUrl(string heroName)
+        => $"https://www.dota2.com/hero/{Regex.Replace(heroName.ToLower(), @"[^a-zA-Z0-9-']", string.Empty)}";
+    public static string GetHeroImage(string internalName)
+        => $"{Hero}{internalName[14..]}.png"; // ignore "npc_dota_hero_" from internal name
+    public static string GetAbilityImage(string internalName)
+        => $"{Ability}{internalName}.png";
+    public static string GetItemImage(string internalName)
+        => $"{Item}{internalName[5..]}.png"; // ignore "item_" from internal name
 
-    public static string GetTeamLogo(int teamId) => $"https://cdn.cloudflare.steamstatic.com/apps/dota2/teamlogos/{teamId}.png";
+    public static string GetTeamLogo(int teamId)
+        => BaseUrl + $"/teamlogos/{teamId}.png";
 
     public static string GetAttributeIcon(this AttributePrimary attribute)
-#pragma warning disable CS8524 // The switch expression does not handle some values of its input type (it is not exhaustive) involving an unnamed enum value.
      => attribute switch
      {
          AttributePrimary.DOTA_ATTRIBUTE_STRENGTH => StrengthIcon,
          AttributePrimary.DOTA_ATTRIBUTE_AGILITY => AgilityIcon,
          AttributePrimary.DOTA_ATTRIBUTE_INTELLECT => IntelligenceIcon,
          AttributePrimary.DOTA_ATTRIBUTE_ALL => UniversalIcon,
+         _ => throw new ArgumentOutOfRangeException(nameof(attribute)),
      };
-#pragma warning restore CS8524 // The switch expression does not handle some values of its input type (it is not exhaustive) involving an unnamed enum value.
 
 }
