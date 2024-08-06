7bcd1aa0266dfec909eeba011a8ef813f2c90a57
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2024-08-06 22:19:34 +0100

    more extension stuff

--- src/UltimyrArchives.Updater/Extensions/KVValueExtensions.cs
+++ src/UltimyrArchives.Updater/Extensions/KVValueExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.Contracts;
+using System.Globalization;
 using ValveKeyValue;
 
 namespace UltimyrArchives.Updater.Extensions;
@@ -8,4 +9,13 @@ public static class KVValueExtensions
     [Pure]
     public static IEnumerable<KVObject>? AsEnumerable(this KVValue kvValue)
         => kvValue as IEnumerable<KVObject>;
+
+    [Pure]
+    public static T ToEnum<T>(this KVValue? kvValue) where T : struct, Enum
+    {
+        if (kvValue is null)
+            return default;
+        Enum.TryParse<T>(kvValue.ToString(CultureInfo.InvariantCulture).Replace('|', ','), true, out var result);
+        return result;
+    }
 }
