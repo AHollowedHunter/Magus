a22a5027da3f986d96f68a84646d319ea945c70b
Ashley Cave <git@acave.dev>
2026-04-03 14:10:29 +0100

    remove redundant call

--- src/Magus.DataBuilder/EntityUpdater.cs
+++ src/Magus.DataBuilder/EntityUpdater.cs
@@ -121,8 +121,6 @@ public class EntityUpdater
         _ItemIds.Clear();
         foreach (var item in abilityIds.Children.Single(x => x.Name == "ItemAbilities").Children.Single(x => x.Name == "Locked"))
         {
-            _ItemIds.Add(item.Name, item.ParseValue<int>());
-
             if (_ItemIds.TryAdd(item.Name, item.ParseValue<int>()) is false)
                 _logger.LogWarning("Possible duplicate item_id for {name}, tried adding {newId} alongside {existingId}", item.Name, item.ParseValue<int>(), _ItemIds[item.Name]);
         }
---
+++ src/UltimyrArchives.Updater/EntityProcessor.cs
@@ -0,0 +1,45 @@
+﻿using Magus.Data.Models.Dota;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using UltimyrArchives.Updater.Converters;
+using UltimyrArchives.Updater.DotaFilePaths;
+using UltimyrArchives.Updater.Utils;
+
+namespace UltimyrArchives.Updater;
+
+internal sealed class PatchNotesProcessor(ILogger<PatchNotesProcessor> logger, GameFileProviderFactory gameFileProviderFactory)
+{
+    public async Task<IReadOnlyList<PatchNote>> GetProcessedAsync(Entity[] entities)
+    {
+        logger.LogInformation("Processing Patch Notes.");
+        var stopwatch = Stopwatch.StartNew();
+
+        var patchNotes = await GetPatchNotesAsync(entities);
+
+        stopwatch.Stop();
+        logger.LogInformation("Finished Processing Patch Notes, took {timeTaken}.", stopwatch.Elapsed);
+
+        return patchNotes;
+    }
+
+    private async Task<PatchNote[]> GetPatchNotesAsync(Entity[] entities)
+    {
+        LocalisedValues localisedValues;
+        KVDocument      patchManifest;
+        using (var gameFileProvider = gameFileProviderFactory.Create())
+        {
+            localisedValues = await new LocalisedValuesBuilder(gameFileProvider)
+                .WithPatchNotes(StringUtils.CleanPatchNote)
+                .BuildAsync()
+                .ConfigureAwait(false);
+            patchManifest = await gameFileProvider.GetPak01KVFileAsync(Pak01.PatchNotes).ConfigureAwait(false);
+        }
+
+        var patchNoteConverter = new PatchNoteConverter();
+        var manifests          = patchManifest.Select(patchNoteConverter.Convert).ToArray();
+
+        // TODO process
+
+        return [];
+    }
+}
